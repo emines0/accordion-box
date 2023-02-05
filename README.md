09adcd1dec460671e2b5dd29bd92c67361262423
emines0 <emin.sefa1@gmail.com>
2023-02-05 12:26:37 +0100

    display data; provide props for SingleQuestion component; change button; display/hide paragraph

--- README.md
+++ README.md
@@ -8,13 +8,4 @@
 
 [react icons](https://react-icons.github.io/react-icons/)
 
-```
-npm install react-icons --save
-```
-
-```javascript
-import { FaHome } from 'react-icons/fa';
-const Component = () => {
-  return <FaHome className='icon'></FaHome>;
-};
-```
+-
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
-};
+import React, { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false)
 
-export default Question;
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {/*
+           * onclick setShowInfo to opposite value
+           */}
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          {/*
+           * if showInfo true display minus else plus button
+           */}
+        </button>
+      </header>
+      {
+        showInfo && <p>{info}</p>
+        /*
+         * if showInfo is true display paragraph with text
+         */
+      }
+    </article>
+  )
+}
+
+export default Question
